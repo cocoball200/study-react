6eb49ba675d3541dd6502c0ec84b7063e0bf2dbb
Mihyeon Byeon <cocoball200@gmail.com>
2020-12-06 15:26:11 +0900

    add component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
 import React from 'react';
+import MyComponent from './MyComponent'
 import './App.css';
 
-
-function App() {
-  const name = '리액트';
-  return (
-    <div
-      style={{
-        backgroundColor: 'black',
-        color: 'pink',
-        fontSize: '48px',
-        fontWeight: 'bold',
-        padding: 20 //단위를 생략하면 px 가 디폴트 
-      }}
-    >
-      {name}
-    </div>
-  );
+const App = () => {
+  return <MyComponent></MyComponent> // or </MyComponent> 
 }
+
 export default App;
---
+++ src/MyComponent.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MyComponent = () => {
+    return <div> 나의 첫 component 생성기</div>
+};
+
+export default MyComponent;
