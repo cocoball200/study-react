ac14112aedbc24e011213fbc7eb75f2905ea5872
Mihyeon Byeon <cocoball200@gmail.com>
2020-12-10 19:33:32 +0900

    임의 의 메서드 만들기

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -4,29 +4,42 @@ class EventPractice extends Component {
     state = {
         message: ''
     }
+
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleChange(e) {
+        alert(this.state.message)
+        this.setState({
+            message: e.target.value
+        });
+    }
+
+    handleClick() {
+        alert(this.state.message)
+        this.setState({
+            message: ''
+        })
+    }
+
     render() {
         return (
             <div>
-                <h1>이벤트 연습</h1>
-                <input type='text' name='message' placeholder='to write something'
-                    value={this.state.message} onChange={
-                        (e) => {
-                            this.setState({
-                                message: e.target.value
-                            })
-                        }
-                    } />
-                <button onClick={
-                    () => {
-                        alert(this.state.message);
-                        this.setState({
-                            message: ''
-                        });
-                    }
-                }>CHECK</button>
+                <h1>Hello React with event</h1>
+                <input
+                    type='text'
+                    name='message'
+                    placeholder='writie'
+                    value={this.state.message}
+                    onChange={this.handleChange}
+                />
+                <button onClick={this.handleClick}>Click</button>
             </div>
         )
     }
-}
 
+}
 export default EventPractice;
