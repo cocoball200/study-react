ef5bcc8619e317e8e20e9b16a28041a83bd79244
Mihyeon Byeon <cocoball200@gmail.com>
2020-12-10 19:02:32 +0900

    how to recorde value when have event

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import MyComponent from './MyComponent'
 import './App.css';
+import EventPractice from './EventPractice';
 
 const App = () => {
-  return (<MyComponent name="Mia " favoritNumber={1004}>
-    MIA </MyComponent>)
+  return (
+    <div>
+      <MyComponent name="Mia " favoritNumber={1004}>
+        MIA </MyComponent>
+      <EventPractice />
+    </div>
+  )
 }
 
 export default App;
---
+++ src/EventPractice.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class EventPractice extends Component {
+    render() {
+        return (
+            <div>
+                <h1>이벤트 연습</h1>
+                <input type='text' name='message' placeholder='to write something'
+                    onChange={
+                        (e) => { console.log(e.target.value) }
+                    }></input>
+            </div>
+        )
+    }
+}
+
+export default EventPractice;
