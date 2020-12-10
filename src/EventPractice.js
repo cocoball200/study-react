22f5d10880392bdb19cdce7c9e2c1f742411fe83
Mihyeon Byeon <cocoball200@gmail.com>
2020-12-10 19:12:49 +0900

    buttom message excute

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
 
 class EventPractice extends Component {
+    state = {
+        message: ''
+    }
     render() {
         return (
             <div>
                 <h1>이벤트 연습</h1>
                 <input type='text' name='message' placeholder='to write something'
-                    onChange={
-                        (e) => { console.log(e.target.value) }
-                    }></input>
+                    value={this.state.message} onChange={
+                        (e) => {
+                            this.setState({
+                                message: e.target.value
+                            })
+                        }
+                    } />
+                <button onClick={
+                    () => {
+                        alert(this.state.message);
+                        this.setState({
+                            message: ''
+                        });
+                    }
+                }>CHECK</button>
             </div>
         )
     }
